1b80f17778c11d959fac770fbb5c62dcfcdb58eb
ketakg <79914911+KetakG@users.noreply.github.com>
2022-08-06 18:08:10 +0530

    Stack DS code

---
+++ Stack.py
@@ -0,0 +1,14 @@
+class Stack:
+
+    def __init__(self):
+        self.stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+    
